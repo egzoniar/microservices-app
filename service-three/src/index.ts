42651984ad3f8aae458c7578efe6d36e2b30bd4b
egzoniar <arifajegzon.ea@gmail.com>
2023-01-23 02:00:24 +0100

    Setup prisma on service three with database tables and migration.

--- service-three/src/index.ts
+++ service-three/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -7,9 +8,36 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.json("Service Three");
+interface Counter {
+  ID: Number;
+  name: String;
+  value: Number;
+}
+
+const db: Counter = {
+  ID: 3,
+  name: "Counter Three",
+  value: 0,
+};
+
+app.get("/", (req, res) => {
+  res.json(db);
 });
+
+app.put("/inc", (req, res) => {
+  db.value += req.body.by;
+
+  serviceOnePublishEvent({
+    event: "UPDATE_COUNTER_THREE",
+    data: db,
+  });
+
+  res.json(db);
+});
+
+async function serviceOnePublishEvent(payload: any) {
+  axios.post("http://localhost:8000/app-event", { payload });
+}
 
 app.listen(PORT, () => {
   console.log(`Service One is listening to port ${PORT}`);
